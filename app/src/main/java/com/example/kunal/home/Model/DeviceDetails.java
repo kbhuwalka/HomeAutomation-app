3935988f8db8db0fa88865c0ac7166d5686e7a23
Kunal Bhuwalka <kunal.bhuwalka@hotmail.com>
2015-08-21 19:08:21 +0530

    Testing VCS on Android Studio

--- app/src/main/java/com/example/kunal/home/Model/DeviceDetails.java
+++ app/src/main/java/com/example/kunal/home/Model/DeviceDetails.java
@@ -6,7 +6,8 @@ package com.example.kunal.home.Model;
 public class DeviceDetails {
     private static String[] mAddress = {
             "A0:F4:50:CD:D9:98",
-            "14:30:C6:16:F8:33"
+            "14:30:C6:16:F8:33",
+            "20:14:03:19:90:73"
     };
 
     public static boolean isValidDevice(String deviceAddress){
